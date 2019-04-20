f6586b6b42326e27b91e6f54fcbe3a411a78fe48
jr3804147 <49802940+jr3804147@users.noreply.github.com>
2019-04-20 13:54:57 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,28 @@
 import 'package:flutter/material.dart';
-							
-void main() => runApp(new MyApp());
-							
-class MyApp extends StatelessWidget {
-@override
+
+void main() => runApp(MyApp());
+
+// This widget is the root of your application.
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => new MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Welcome to Flutter'),
+    // set material design app
+    return MaterialApp(
+      title: 'solocoding2019', // application name
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Demo'), // app bar title
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: Center(
+          child: Text('Hello, world'), // center text
         ),
       ),
     );
